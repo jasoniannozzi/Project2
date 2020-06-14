a7c84387cf739e8183f4f468c8c889d4fcbcedb5
Jiannozzi <Jasoniannozzi@icloud.com>
2020-06-13 20:06:23 -0400

    new background

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
-showResults = function(results) {
-    
-    let html = "";
-    results.map(function(result) {
-      console.log(result.images.original.url);
-      html = html + `<img class='imgresults' src=${result.images.original.url} />`
+showResults = function (results) {
+
+  let html = "";
+  results.map(function (result) {
+    console.log(result.images.original.url);
+    html = html + `<img class='imgresults' src=${result.images.original.url} />`
+  });
+  console.log(html);
+  $('#results').html("")
+  $('#results').append(html);
+
+};
+
+const fetchSearchResults = function (searchValue) {
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=KA4bCwoX66gmvt7ogoGShUai2L59BFaQ&q=${searchValue}`;
+  console.log(url);
+  fetch(url)
+    .then(function (res) {
+      return res.json();
+    }).then(function (response) {
+      // console.log(response.data);
+      showResults(response.data);
     });
-    console.log(html);
-    $('#results').html("")
-    $('#results').append(html);
-  
-  }; 
-  
-  const fetchSearchResults = function(searchValue) {
-    let url = `https://api.giphy.com/v1/gifs/search?api_key=KA4bCwoX66gmvt7ogoGShUai2L59BFaQ&q=${searchValue}`;
-    console.log(url);
-    fetch(url)
-      .then(function(res) {
-        return res.json();
-      }).then(function(response) {
-        // console.log(response.data);
-        showResults(response.data);
-      });
-  
-  }
-  
-  const bootUp = function() {
-    
-    $('#search').on('click', function(event){
-      let searchValue = $('#search-box').val();
-      console.log(searchValue);
-      fetchSearchResults(searchValue);
-  
-    });
-  
-  
-  }
-  
-  $(bootUp) 
+
+}
+
+const bootUp = function () {
+
+  $('#search').on('click', function (event) {
+    let searchValue = $('#search-box').val();
+    console.log(searchValue);
+    fetchSearchResults(searchValue);
+
+  });
+
+
+}
+
+$(bootUp)
 
